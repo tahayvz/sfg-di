ffd9348dc78f84c9b96e4eb898b51b7a0228d13b
Taha Yavuz <67894267+tahabusiness@users.noreply.github.com>
2021-07-12 08:09:20 +0300

    adding context example

---
+++ src/main/java/com/tahayvz/sfgdi/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.tahayvz.sfgdi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World!!");
+
+        return "Hi Folks!";
+    }
+}
