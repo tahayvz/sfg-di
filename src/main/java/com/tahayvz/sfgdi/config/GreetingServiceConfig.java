11f68059e19106986fd6aee45e38fe03c9e55e08
Taha Yavuz <67894267+tahabusiness@users.noreply.github.com>
2021-07-12 11:17:40 +0300

    adding primary and profile examples

--- src/main/java/com/tahayvz/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/tahayvz/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,37 @@
 package com.tahayvz.sfgdi.config;
 
-import com.tahayvz.sfgdi.services.ConstructorInjectedGreetingService;
-import com.tahayvz.sfgdi.services.PropertyInjectedGreetingService;
-import com.tahayvz.sfgdi.services.SetterInjectedGreetingService;
+import com.tahayvz.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"TR", "default"})
+    @Bean("i18NTurkishGreetingService")
+    I18NTurkishGreetingService i18NTurkishGreetingService(){
+        return new I18NTurkishGreetingService();
+    }
+
+    @Profile("ES")
+    @Bean("i18nService")
+    I18NSpanishGreetingService i18NSpanishService(){
+        return new I18NSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorInjectedGreetingService constructorInjectedGreetingService(){
